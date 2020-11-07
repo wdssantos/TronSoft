a9624ed4492a5e8b6a4c53782fc80f66b560efd9
wdssantos <36804151+wdssantos@users.noreply.github.com>
2020-11-07 11:04:48 -0300

    Up

---
+++ Development/RX/DX/Bundles/rtl/Kernel/Sources/tftData.Messages.pas
@@ -0,0 +1,19 @@
+{ *********************************************************** }
+{ *                                                         * }
+{ *  TronSoft - Desenvolvimento de Software Especializado.  * }
+{ *                                                         * }
+{ *              Copyright(c) 2020 TronSoft.                * }
+{ *                                                         * }
+{ *********************************************************** }
+
+unit tftData.Messages;
+
+interface
+
+resourcestring
+  SRequiredField = 'O campo ''%s'' é de preenchemento obrigatório.';
+
+implementation
+
+end.
+
